2da1616be30a05d26294145f4de1484f4bc53e6c
NguyenVanThinhAn <deleterobo2@gmail.com>
2026-04-21 21:40:51 +0700

    Add files via upload

--- BTTH.sql
+++ BTTH.sql
@@ -85,7 +85,7 @@ INSERT INTO bookings (revenue, status, userID, hotelID) VALUES
 
 -- An
 (1200000, 'COMPLETED', 1, 1),
-(2500000, 'COMPLETED', 1, 2),
+(250000000, 'COMPLETED', 1, 2),
 (1800000, 'CANCELLED', 1, 3),
 (900000,  'COMPLETED', 1, 4),
 
@@ -96,37 +96,38 @@ INSERT INTO bookings (revenue, status, userID, hotelID) VALUES
 (800000,  'COMPLETED', 2, 5),
 
 -- Chi
-(900000,  'COMPLETED', 3, 5),
+(50000000,  'COMPLETED', 3, 5),
 (-1100000, 'CANCELLED', 3, 2),
-(4000000, 'COMPLETED', 3, 1),
+(50000000, 'COMPLETED', 3, 1),
 (700000,  'COMPLETED', 3, 4),
 
 -- Dũng
 (2700000, 'COMPLETED', 4, 4),
 (3000000, 'COMPLETED', 4, 2),
-(1300000, 'CANCELLED', 4, 5),
+(500000000, 'CANCELLED', 4, 5),
 (1500000, 'COMPLETED', 4, 1),
 
 -- Hà
 (2200000, 'COMPLETED', 5, 1),
 (-2600000, 'CANCELLED', 5, 3),
-(3100000, 'COMPLETED', 5, 2),
+(500000000, 'COMPLETED', 5, 2),
 (-1000000, 'COMPLETED', 5, 4),
 
 -- DATA BẨN (NULL CASES)
 (NULL,    'COMPLETED', 1, 5),
-(500000,  NULL,        2, 3),
+(5000000,  NULL,        2, 3),
 (1800000, 'COMPLETED', NULL, 2),
 (1400000, 'CANCELLED', 3, NULL),
 (NULL,    NULL,        NULL, NULL);
 
-SELECT hotels.name AS "Khách sạn", hotels.rate AS "Sao(rate)", SUM(bookings.revenue) AS "Doanh thu",
-COUNT(CASE WHEN bookings.status = "COMPLETED" THEN 1 END) AS "Đặt(Đã thanh toán)",
-COUNT(CASE WHEN bookings.status = "CANCELLED" THEN 1 END) AS "Hủy"
+SELECT users.name AS "Khách hàng", hotels.rate AS "Hạng sao", SUM(bookings.revenue) AS "Tổng chi tiêu"
 FROM users
 JOIN bookings ON bookings.userID = users.id
 JOIN hotels ON hotels.id = bookings.hotelID
-GROUP BY hotels.id;
+WHERE bookings.status = 'COMPLETED'AND bookings.revenue IS NOT NULL
+GROUP BY users.id, hotels.rate
+HAVING SUM(bookings.revenue) > 50000000
+ORDER BY hotels.rate DESC, SUM(bookings.revenue) DESC;
 
 -- Em dùng id của hotel để lọc ra, rate cũng đi theo hotel, tổng doanh thu dùng hàm SUM và vì booking id cũng được link với id của hotel từ đó tính toán SUM chính xác. Booking lại là khóa ngoại và cũng được link với USERID, từ đó 1 người dùng hay doanh thu từ người dùng đó có thể xuất hiện trên nhiều khách sạn tùy thuộc vào người dùng đó book khách sạn nào
 -- Mặc định nó đã không được tính vào doanh thu vì đơn giản nó đã bị CANCELLED, em đã lọc ra COMPLETED để tính. Thế nhưng nếu phải lọc thì em sẽ lọc ở WHERE, vì nó đơn giản và tối ưu hơn thay vì gom lại rồi lọc. Hơn nữa, với INNER JOIN, các trường hợp không hợp lệ đều đã bị clean.
